cd562c269ace8fb6744ee6cc68eb525ab68dc7fb
Niels Müller <niels.mueller@physik.tu-darmstadt.de>
2016-06-13 11:34:33 +0200

    Generate xtc index in pygmx.open if necessary.

--- pygmx/__init__.py
+++ pygmx/__init__.py
@@ -2,11 +2,14 @@
 Python wrapper for the gromacs library.
 """
 
+import os
+
 from .tpxio import TPXReader
 from .xtcio import XTCReader
 # from .enxio import EDRFile
 from .errors import FileTypeError
 from .gromacs.reader import index_filename_for_xtc
+from .gromacs.xtcindex import index_xtcfile
 
 FILE_EXTENSIONS = {
     'xtc': XTCReader,
@@ -20,6 +23,11 @@ def open(filename):
     if ext in FILE_EXTENSIONS:
         if ext in ['xtc']:
             indexfile = index_filename_for_xtc(filename)
+            if not os.path.exists(indexfile):
+                print('Gneerating Index for xtc file. This may take a while...')
+                index_xtcfile(filename)
             return FILE_EXTENSIONS[ext](filename, indexfile)
+        else:
+            return FILE_EXTENSIONS[ext](filename)
     else:
         raise FileTypeError('Filetype {} not supported by pygmx.'.format(ext))
