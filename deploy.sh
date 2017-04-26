3cbbdb7c753049aff0ecef17f4eff9cd7be1d7af
Niels Müller <niels.mueller@physik.tu-darmstadt.de>
2017-04-26 12:39:44 +0200

    Deploy Script for nfsopt

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,17 @@
 #!/bin/bash -l
 
-module purge
-module load gromacs/5.1.2
 module load mdevaluate/dev
+python --version
+TMPDIR="./tmp"
+PYVERSION="$(python -c 'import sys; print("python{}.{}".format(sys.version_info.major, sys.version_info.minor))')"
+PYLIBDIR="$TMPDIR/lib/$PYVERSION/site-packages"
 
-python setup.py install --prefix=/data/niels/modules/mdevaluate-dev
+rm -rf $TMPDIR
+mkdir -p $PYLIBDIR
+
+PYTHONPATH="$PYLIBDIR:$PYTHONPATH"
+
+if PYTHONPATH="$PYLIBDIR:$PYTHONPATH" python setup.py install --prefix=$TMPDIR
+then
+  scp -r $PYLIBDIR niels@nas2:/nfsopt/mdevaluate/mdevaluate-dev/lib/$PYVERSION/site-packages
+fi
