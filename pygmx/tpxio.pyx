b033245b12563a622ee80e57764cd42337ccf3a1
Niels Müller <post@nielsm.de>
2019-10-24 12:48:57 +0200

    Remoce Arg type

--- pygmx/tpxio.pyx
+++ pygmx/tpxio.pyx
@@ -254,7 +254,7 @@ cdef class TPXReader:
 
 
 @cython.binding(True)
-def make_xtcframe_whole(coords, box, <TPXReader>reader):
+def make_xtcframe_whole(coords, box, reader):
     cdef t_atoms = gmx_mtop_global_atoms(reader.topology)
     cdef np.ndarray[real, ndim=2] b = np.asarray(box, dtype=np.float32)
     cdef np.ndarray[real, ndim=2] x = np.asarray(coords, dtype=np.float32)
